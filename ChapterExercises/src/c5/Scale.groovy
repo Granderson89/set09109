7079dc40413734c3c43ae57a5622cd3c37eef7de
Anderson <40218994@live.napier.ac.uk>
2018-01-24 12:01:09 +0000

    Exercises 5-1, 5-2

--- ChapterExercises/src/c5/Scale.groovy
+++ ChapterExercises/src/c5/Scale.groovy
@@ -6,6 +6,7 @@ import c05.ScaledData
    
 class Scale implements CSProcess {
   def int scaling
+  def int multiplier = 2
   def ChannelOutput outChannel
   def ChannelOutput factor
   def ChannelInput inChannel
@@ -36,17 +37,46 @@ class Scale implements CSProcess {
     while (true) {
       switch ( scaleAlt.priSelect(preCon) ) {
         case SUSPEND :
-          //  deal with suspend input        
+          //  deal with suspend input
+		  suspended = true
+		  suspend.read()
+		  factor.write(scaling)
+		  println "Suspended"
+		  preCon[SUSPEND] = false
+		  preCon[INJECT] = true
+		  preCon[TIMER] = false
+		  preCon[INPUT] = true
           break
         case INJECT:
           //  deal with inject input
+		  scaling = injector.read()
+		  println "Injected scaling is $scaling"
+		  suspended = false
+		  timeout = timer.read() + DOUBLE_INTERVAL
+		  timer.setAlarm(timeout)
+		  preCon[SUSPEND] = true
+		  preCon[INJECT] = false
+		  preCon[TIMER] = true
+		  preCon[INPUT] = true
           break
         case TIMER:
           //  deal with Timer input
-          println "Normal Timer: new scaling is ${scaling}"
+          timeout = timer.read() + DOUBLE_INTERVAL
+		  timer.setAlarm(timeout)
+		  scaling = scaling * multiplier
+		  println "Normal Timer: new scaling is ${scaling}"
+
           break
         case INPUT:
-          //   deal with Input channel 
+          //   deal with Input channel
+		  def inValue = inChannel.read()
+		  def result = new ScaledData()
+		  result.original = inValue
+		  if (suspended == true)
+			  result.scaled = inValue
+		  else
+			  result.scaled = inValue * scaling
+		  outChannel.write(result)
           break
       } //end-switch
     } //end-while
